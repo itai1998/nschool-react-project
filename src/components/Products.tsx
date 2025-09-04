aea759e99bfc95b86e849e6d5fc9d9a7d0bc28fb
I-Tai Lin <smes93434@gmail.com>
2025-09-04 23:58:45 +0800

    products layout

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,48 @@
+import '../scss/Product.scss'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+
 export default function Products() {
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToScroll: 1,
+    slidesToShow: 4,
+    arrows: true,
+    prevArrow: (
+      <button type="button" className="slick-prev">
+        Previous
+      </button>
+    ),
+    nextArrow: (
+      <button type="button" className="slick-next">
+        Next
+      </button>
+    ),
+  }
   return (
-    <div>
-      <h3>Products</h3>
+    <div className="productsContainer">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
     </div>
   )
 }
