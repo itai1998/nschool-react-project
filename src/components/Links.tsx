400e812552ffc3b5e0cac463101a1175cc3dd662
I-Tai Lin <smes93434@gmail.com>
2025-09-05 00:26:35 +0800

    create Links layout

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,7 +1,15 @@
+import '../scss/Links.scss'
+
 export default function Links() {
   return (
-    <div>
+    <div className="linksContainer">
       <h3>Links</h3>
+      <div className="links">
+        <button>Link 1</button>
+        <button>Link 2</button>
+        <button>Link 3</button>
+        <button>Link 4</button>
+      </div>
     </div>
   )
 }
