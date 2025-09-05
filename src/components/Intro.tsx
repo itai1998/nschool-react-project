b8fced16ab75aee28c822c85040ad6a2ee99bc56
I-Tai Lin <smes93434@gmail.com>
2025-09-05 23:43:57 +0800

    add intro style

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,14 +1,32 @@
 import '../scss/Intro.scss'
+import avatar from '../img/avatar.jpg'
+import contactIcon from '../img/communication.png'
 
 export default function Intro() {
   return (
     <div className="introContainer">
       <div className="favorites">
-        <h3>Text left</h3>
+        <h1>
+          <span>商店。</span> 購買你喜愛的產品，
+          <br />
+          這是最好的方式。
+        </h1>
       </div>
-      <div className="contacts">
-        <h3>Text right 1</h3>
-        <h3>Text right 2</h3>
+      <div className="contactContainer">
+        <div className="contactCard">
+          <img src={avatar} alt="avatar" />
+          <div className="contact left">
+            <h5>需要購物協助嗎？</h5>
+            <h5>洽詢專員</h5>
+          </div>
+        </div>
+        <div className="contactCard">
+          <img src={contactIcon} alt="contactIcon" />
+          <div className="contact">
+            <h5>前往 Apple 直營店</h5>
+            <h5>尋找附近的直營店</h5>
+          </div>
+        </div>
       </div>
     </div>
   )
