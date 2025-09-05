518eb2a0e6b03ed7d15b60c96c79e17cf9f76c31
I-Tai Lin <smes93434@gmail.com>
2025-09-05 22:53:40 +0800

    add navBar style

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,36 @@
 import '../scss/NavBar.scss'
+import appleLogo from '../img/apple-logo.png'
+import searchIcon from '../img/search-interface-symbol.png'
+import marketIcon from '../img/market.png'
 
 export default function NavBar() {
   const navOptions = [
-    'icon1',
-    'icon2',
-    'icon3',
-    'icon4',
-    'icon5',
-    'icon6',
-    'icon7',
-    'icon8',
-    'icon9',
-    'icon10',
-    'icon11',
-    'icon12',
-    'icon13',
+    { type: 'icon', src: appleLogo, alt: 'Apple Logo' },
+    { type: 'text', content: '商店' },
+    { type: 'text', content: 'Mac' },
+    { type: 'text', content: 'iPad' },
+    { type: 'text', content: 'iPhone' },
+    { type: 'text', content: 'Watch' },
+    { type: 'text', content: 'Vision' },
+    { type: 'text', content: 'AirPods' },
+    { type: 'text', content: 'TV 和家庭' },
+    { type: 'text', content: '娛樂' },
+    { type: 'text', content: '配件' },
+    { type: 'text', content: '支援服務' },
+    { type: 'icon', src: searchIcon, alt: 'Search' },
+    { type: 'icon', src: marketIcon, alt: 'Market' },
   ]
 
   return (
     <div className="container">
       <div className="iconContain">
-        {navOptions.map((icon) => (
-          <button key={icon}>{icon}</button>
-        ))}
+        {navOptions.map((item, idx) =>
+          item.type === 'icon' ? (
+            <img key={idx} src={item.src} alt={item.alt} />
+          ) : (
+            <span key={idx}>{item.content}</span>
+          )
+        )}
       </div>
     </div>
   )
