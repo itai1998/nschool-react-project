123053f82376648eb4954c08cf4fe08bee7be25f
I-Tai Lin <smes93434@gmail.com>
2025-09-06 10:33:48 +0800

    add style in products

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,36 +2,97 @@ import '../scss/Product.scss'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
+import mac from '../img/mac.png'
+import iphone from '../img/iphone.png'
+import ipad from '../img/ipad.png'
+import appleWatch from '../img/appleWatch.png'
+import appleVisionPro from '../img/visionPro.png'
+import airPods from '../img/airPods.png'
+import airTag from '../img/airTag.png'
+import homePod from '../img/homePod.png'
+import accessories from '../img/others.png'
+import appleStoreGiftCard from '../img/appleStore.png'
 
 export default function Products() {
   const settings = {
     infinite: false,
     speed: 500,
     slidesToScroll: 1,
-    slidesToShow: 4,
+    slidesToShow: 9,
     arrows: true,
+    prevArrow: (
+      <button type="button" className="slick-prev">
+        Previous
+      </button>
+    ),
+    nextArrow: (
+      <button type="button" className="slick-next">
+        Next
+      </button>
+    ),
   }
+
+  const products = [
+    {
+      displayName: 'Mac',
+      image: mac,
+      url: '/',
+    },
+    {
+      displayName: 'iPhone',
+      image: iphone,
+      url: '/',
+    },
+    {
+      displayName: 'iPad',
+      image: ipad,
+      url: '/',
+    },
+    {
+      displayName: 'Apple Watch',
+      image: appleWatch,
+      url: '/',
+    },
+    {
+      displayName: 'Apple Vision Pro',
+      image: appleVisionPro,
+      url: '/',
+    },
+    {
+      displayName: 'AirPods',
+      image: airPods,
+      url: '/',
+    },
+    {
+      displayName: 'AirTag',
+      image: airTag,
+      url: '/',
+    },
+    {
+      displayName: 'HomePod',
+      image: homePod,
+      url: '/',
+    },
+    {
+      displayName: '配件',
+      image: accessories,
+      url: '/',
+    },
+    {
+      displayName: 'Apple Store 禮品卡',
+      image: appleStoreGiftCard,
+      url: '/',
+    },
+  ]
   return (
     <div className="productsContainer">
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        {products.map((product) => (
+          <div key={product.displayName}>
+            <img src={product.image} alt={product.displayName} />
+            <p>{product.displayName}</p>
+          </div>
+        ))}
       </Slider>
     </div>
   )
