e15a78d5acf5c2670def22b850d38b6589e7b6fb
I-Tai Lin <smes93434@gmail.com>
2025-09-07 17:03:51 +0800

    create Product card

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import Intro from './components/Intro'
 import Links from './components/Links'
 import NavBar from './components/NavBar'
 import NewProducts from './components/NewProducts'
+import ProductCard from './components/ProductCard'
 import Products from './components/Products'
 import StoreInfo from './components/StoreInfo'
+import iphonePink from './img/productImg/iphonePink.jpg'
 
 function App() {
   return (
@@ -17,6 +19,12 @@ function App() {
       <NewProducts />
       <HelperOptions />
       <StoreInfo />
+      <ProductCard
+        img={iphonePink}
+        label="Apple"
+        title="iPhone 16"
+        price="NT$30,000"
+      />
       <Links />
       <Footer />
     </div>
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,24 @@
+import '../scss/ProductCard.scss'
+
+interface ProductCardProps {
+  img: string
+  label?: string
+  title: string
+  price: string
+}
+
+export default function ProductCard({
+  img,
+  label = '',
+  title,
+  price,
+}: ProductCardProps) {
+  return (
+    <div className="productCardContainer">
+      <img src={img} alt="product" />
+      <p className="label">{label}</p>
+      <h3 className="title">{title}</h3>
+      <h4 className="price">{price}</h4>
+    </div>
+  )
+}
