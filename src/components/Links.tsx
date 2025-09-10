da34b2df58d0f6d59c7137dcb7f6c3d4d5af6f8c
I-Tai Lin <smes93434@gmail.com>
2025-09-10 22:51:15 +0800

    create links style

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -3,12 +3,12 @@ import '../scss/Links.scss'
 export default function Links() {
   return (
     <div className="linksContainer">
-      <h3>Links</h3>
+      <h2>快速連結</h2>
       <div className="links">
-        <button>Link 1</button>
-        <button>Link 2</button>
-        <button>Link 3</button>
-        <button>Link 4</button>
+        <button>尋找商店</button>
+        <button>訂單狀態</button>
+        <button>購物協助</button>
+        <button>你的收藏</button>
       </div>
     </div>
   )
