04863fad95aa2261f3a8db09d40768faa59ec4b6
I-Tai Lin <smes93434@gmail.com>
2025-09-14 00:36:56 +0800

    remove all the border

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,13 +2,15 @@ import '../scss/Links.scss'
 
 export default function Links() {
   return (
-    <div className="linksContainer">
-      <h2>快速連結</h2>
-      <div className="links">
-        <button>尋找商店</button>
-        <button>訂單狀態</button>
-        <button>購物協助</button>
-        <button>你的收藏</button>
+    <div className="linksWrapper">
+      <div className="linksContainer">
+        <h2>快速連結</h2>
+        <div className="links">
+          <button>尋找商店</button>
+          <button>訂單狀態</button>
+          <button>購物協助</button>
+          <button>你的收藏</button>
+        </div>
       </div>
     </div>
   )
