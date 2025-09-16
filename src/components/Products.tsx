e4969811dae2ea0102c5994640b6b6818f8c8e6f
I-Tai Lin <smes93434@gmail.com>
2025-09-17 00:03:57 +0800

    fix the screen samll size issue for Intro

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -30,6 +30,43 @@ export default function Products() {
         Next
       </button>
     ),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
 
   const products = [
