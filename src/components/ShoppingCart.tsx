325fe1143995324c72434c60e2a22c63b7da549f
I-Tai Lin <smes93434@gmail.com>
2025-10-01 23:17:49 +0800

    use useForm to handle form logic

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,19 +1,46 @@
+import { useForm } from "react-hook-form";
 import styles from "../scss/ShoppingCart.module.scss";
 
+interface LoginData {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}
+
 export default function ShoppingCart() {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+  });
+
+  const onSubmit = (data: LoginData) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.shoppingCartContainer}>
       <h1>登入以加快結帳速度。</h1>
       <div className={styles.loginContainer}>
         <h2>登入 Apple Store</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.inputContainer}>
-            <input type="text" placeholder="電子郵件地址或電話號碼" />
-            <input type="password" placeholder="密碼" />
+            <input
+              {...register("email")}
+              type="text"
+              placeholder="電子郵件地址或電話號碼"
+            />
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="密碼"
+            />
           </div>
 
           <div className={styles.checkboxContainer}>
-            <input type="checkbox" />
+            <input {...register("rememberMe")} type="checkbox" />
             <h4>記住我的帳號</h4>
           </div>
           <button type="submit" className={styles.loginButton}>
