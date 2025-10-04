a425989804fcd13393cdd1cfd0780c0a8272232f
I-Tai Lin <smes93434@gmail.com>
2025-10-04 12:08:41 +0800

    create logged in section

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import styles from "../scss/ShoppingCart.module.scss";
+import { useState } from "react";
 
 interface LoginData {
   email: string;
@@ -8,6 +9,8 @@ interface LoginData {
 }
 
 export default function ShoppingCart() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const { register, handleSubmit } = useForm({
     defaultValues: {
       email: "",
@@ -17,37 +20,53 @@ export default function ShoppingCart() {
   });
 
   const onSubmit = (data: LoginData) => {
+    setIsLoggedIn(true);
     console.log(data);
   };
 
   return (
     <div className={styles.shoppingCartContainer}>
       <h1>登入以加快結帳速度。</h1>
-      <div className={styles.loginContainer}>
-        <h2>登入 Apple Store</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className={styles.inputContainer}>
-            <input
-              {...register("email")}
-              type="text"
-              placeholder="電子郵件地址或電話號碼"
-            />
-            <input
-              {...register("password")}
-              type="password"
-              placeholder="密碼"
-            />
-          </div>
+      {!isLoggedIn && (
+        <div className={styles.loginContainer}>
+          <h2>登入 Apple Store</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.inputContainer}>
+              <input
+                {...register("email")}
+                type="text"
+                placeholder="電子郵件地址或電話號碼"
+              />
+              <input
+                {...register("password")}
+                type="password"
+                placeholder="密碼"
+              />
+            </div>
 
-          <div className={styles.checkboxContainer}>
-            <input {...register("rememberMe")} type="checkbox" />
-            <h4>記住我的帳號</h4>
-          </div>
-          <button type="submit" className={styles.loginButton}>
-            登入
+            <div className={styles.checkboxContainer}>
+              <input {...register("rememberMe")} type="checkbox" />
+              <h4>記住我的帳號</h4>
+            </div>
+            <button type="submit" className={styles.loginButton}>
+              登入
+            </button>
+          </form>
+        </div>
+      )}
+
+      {isLoggedIn && (
+        <div className={styles.loggedInContainer}>
+          <h2>已登入</h2>
+          <button
+            type="button"
+            className={styles.logoutButton}
+            onClick={() => setIsLoggedIn(false)}
+          >
+            登出
           </button>
-        </form>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
