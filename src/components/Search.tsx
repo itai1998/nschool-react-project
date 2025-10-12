fd19008288ccdc4f9b9a6cc23e757d3c7dcd29d9
I-Tai Lin <smes93434@gmail.com>
2025-10-12 17:18:49 +0800

    show the result when clicking the search button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -31,6 +31,7 @@ export default function Search() {
   const [search, setSearch] = useState("");
   const [open, setOpen] = useState(false);
   const [debouncedSearch, setDebouncedSearch] = useState(search);
+  const [results, setResults] = useState<Product[]>();
 
   useEffect(() => {
     const debounce = setTimeout(() => {
@@ -50,16 +51,27 @@ export default function Search() {
       const res = await axios.get<MockData>("/src/data/macbookMock.json");
       setData(res.data);
 
+      // Show all macbooks initially
+      if (!results) {
+        setResults(res.data.macbooks);
+      }
+
       const filteredSuggestions = res.data.macbooks
         .filter((item) =>
           item.name.toLowerCase().includes(debouncedSearch.toLowerCase())
         )
         .map((item) => item.name);
-
       setSuggestions(filteredSuggestions);
     };
     fetchData();
   }, [debouncedSearch]);
+
+  const handleSearch = (inputText: string) => {
+    const filteredResults = data?.macbooks.filter((item) =>
+      item.name.toLowerCase().includes(inputText.toLowerCase())
+    );
+    setResults(filteredResults || []);
+  };
 
   return (
     <div>
@@ -72,7 +84,14 @@ export default function Search() {
             placeholder="Search"
             onChange={(e) => setSearch(e.target.value)}
           />
-          <button className={styles.searchButton} type="button">
+          <button
+            className={styles.searchButton}
+            type="button"
+            onClick={() => {
+              handleSearch(search);
+              setSearch("");
+            }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="18"
@@ -102,17 +121,17 @@ export default function Search() {
       </div>
 
       <div className={styles.flexContainer}>
-        {data?.macbooks
-          .filter((item) =>
-            item.name.toLowerCase().includes(debouncedSearch.toLowerCase())
-          )
-          .map((item) => (
+        {results && results.length > 0 ? (
+          results.map((item) => (
             <div key={item.name} className={styles.productBox}>
               <h2>{item.name}</h2>
               <h3>{item.price}</h3>
               <h4>{item.type}</h4>
             </div>
-          ))}
+          ))
+        ) : results && results.length === 0 ? (
+          <p>No products found</p>
+        ) : null}
       </div>
     </div>
   );
